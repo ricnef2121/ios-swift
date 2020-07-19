e5ab62aa36f1d227cc31ef941ba0706c3ba92499
¨ricnef2121¨ <ricnef2121@gmail.com>
2020-07-19 13:30:08 -0500

    first commit

---
+++ TuColor/TuColor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TuColor
+//
+//  Created by Daniel on 19/07/20.
+//  Copyright © 2020 Ric. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    //conector de la vista
+    @IBOutlet weak var colorView: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        colorView.backgroundColor = .black
+    }
+//acciones
+    //cambio de boton
+    @IBAction func swichCambio(_ sender: UISwitch) {
+        colorView.backgroundColor = .red
+    }
+    
+}
+
